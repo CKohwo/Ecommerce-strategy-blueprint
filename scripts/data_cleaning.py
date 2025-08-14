ab26111e43d19279af943accfb9d481ac0d0de47
charles onokohwomo <142623911+CKohwo@users.noreply.github.com>
2025-08-14 18:46:16 +0000

    Phase 2: Data Quality & Consistency Audit

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -1,1 +1,50 @@
+import pandas as pd
+from pathlib import path
 
+#Making sure the dataframe is logically correct, actual price must be greater or equal to discounted price
+def price_check(df, issues):
+    invalid_rows = df[df["discounted_price"] > df["actual_price"]]
+    if not invalid_rows.empty:
+        issues["invalid_price"] = invalid_rows
+    return issues    
+
+#Checking for logical error in the ratings, rating must not be lower than 0 
+def ratings_check(df, issues):
+    invalid_neg = df[df["ratings"] < 0]
+    if not invalid_neg.empty:
+        issues["rating_below_0"] = invalid_neg
+         
+#Checking for logical error in the ratings, rating must not exceed 5  
+    invalid_high = df[df["ratings"] > 5]
+    if not invalid_high.empty:
+        issues["ratings_above_5"] = invalid_high   
+    return issues    
+
+# checking for unrealistic discount(>90%) in the dataframe      
+def check_discount(df, issues):
+    unrealistic_discount = df[df["discount_percentage_num"] > 90]
+    if not unrealistic_discount.empty:
+        issues["unrealstics_dis"] = unrealistic_discount 
+    return issues    
+
+
+#Checking for duplicates within the dataframe
+def check_duplicates(df, issues):
+    duplicates = df[df.duplicated()]
+    if not duplicates.empty:
+        issues["duplicates"] = duplicates 
+    return issues           
+
+
+def data_cleaning(df, export_path = None ):
+    issues = {}
+    issues = price_check(df, issues)
+    issues = ratings_check(df, issues)
+    issues = check_discount(df, issues)
+    issues = check_duplicates(df, issues)
+     
+    if export_path:
+        for name, data in issues.items():
+            data.to_csv(f"{export_path}_{name}.csv", index=False)
+
+    return issues
